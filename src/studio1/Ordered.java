51b77d51d4aaedc3d050221150627d44d79c172e
ariannaschwartz <ariannaschwartz@localhost>
2023-09-06 14:15:53 -0500

    Studio 1

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,10 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		
+	boolean isOrdered = ((x < y) && (y < z))||((x > y) && (y > z));
+	// true if three values are either x<y<z or x>y>z
+	System.out.println(isOrdered); 
 	}
 
 }
